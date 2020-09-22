eb269313793e39fb9a270c392327e8db25e65908
Grafelhaft <mg.markusgraf@outlook.com>
2020-09-22 23:06:28 +0200

    optimize imports

--- app/src/main/java/de/grafelhaft/textviewvalidator/MainActivity.java
+++ app/src/main/java/de/grafelhaft/textviewvalidator/MainActivity.java
@@ -1,19 +1,12 @@
 package de.grafelhaft.textviewvalidator;
 
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-import android.text.Editable;
-import android.view.View;
-
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
