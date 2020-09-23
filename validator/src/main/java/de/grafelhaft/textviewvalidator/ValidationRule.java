a1ece224779f6e94fda9554252057caff4cc4586
Grafelhaft <mg.markusgraf@outlook.com>
2020-09-23 19:23:43 +0200

    delete unused class

--- validator/src/main/java/de/grafelhaft/textviewvalidator/ValidationRule.java
+++
@@ -1,8 +0,0 @@
-package de.grafelhaft.textviewvalidator;
-
-import android.text.Editable;
-import android.widget.TextView;
-
-public interface ValidationRule {
-    boolean onTextChanged(TextView view, Editable editable);
-}
