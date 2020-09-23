aaf6d19275bd7b4382fdabae773470f0ab1813bc
Grafelhaft <mg.markusgraf@outlook.com>
2020-09-23 18:30:05 +0200

    clean up packages

--- app/src/main/java/de/grafelhaft/textviewapp/MainActivity.java
+++ app/src/main/java/de/grafelhaft/textviewapp/MainActivity.java
@@ -1,4 +1,4 @@
-package de.grafelhaft.textviewvalidator;
+package de.grafelhaft.textviewapp;
 
 import android.os.Bundle;
 import android.view.Menu;
@@ -7,6 +7,8 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
+import de.grafelhaft.textviewvalidator.TextViewValidator;
 
 public class MainActivity extends AppCompatActivity {
 
